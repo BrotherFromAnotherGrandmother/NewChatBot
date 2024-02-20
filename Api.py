e7201b114d41b2ba9974174be39f493e2e85fc9b
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2024-02-20 17:18:37 +0300

    получил список всех проверенных работ через long_polling

--- Api.py
+++ Api.py
@@ -1,1 +1,14 @@
+from pprint import pprint
+
 import requests
+
+headers = {
+    "Authorization": "Token 6ebf52cd576855857e60303b91c292a6cab40497"
+}
+
+url = 'https://dvmn.org/api/long_polling/'
+response = requests.get(url, headers=headers)
+
+
+print(response)
+pprint(response.json())
