ed4ab9b22ba7bc56887a56124ee9191155bad9b5
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2024-02-21 13:19:18 +0300

    Кастомизировал сообщения

--- Api.py
+++ Api.py
@@ -16,7 +16,20 @@ while True:
         payload = {"timestamp": current_timestamp}
         response = requests.get(url, headers=headers, timeout=5, params=payload)
         current_timestamp = response.json()['last_attempt_timestamp']
-        bot.send_message(chat_id=2131163741, text=f"Преподаватель проверил работу!")
+
+        if response.json()['new_attempts'][0]['is_negative'] == True:
+            bot.send_message(chat_id=2131163741, text=f'''
+            Преподаватель проверил работу! {response.json()['new_attempts'][0]['lesson_title']}
+            Ну ты внатуре не баклажан. И нихуя ты не кабан
+            Непррррравильно, ёбаные волки!
+            Ссылка на урок: {response.json()['new_attempts'][0]['lesson_url']}
+            ''')
+        else:
+            bot.send_message(chat_id=2131163741, text=f'''
+            Преподаватель проверил работу! {response.json()['new_attempts'][0]['lesson_title']}
+            Ты анакондовый Джанго! Всё правильно? Ну естеееесвенно!
+            Ссылка на урок: {response.json()['new_attempts'][0]['lesson_url']}''')
+
     except requests.exceptions.ReadTimeout:
         continue
     except requests.exceptions.ConnectionError:
