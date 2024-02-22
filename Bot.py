1f08eb2edd41a6e936ccc740c146a02d8442e9d2
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2024-02-22 13:46:29 +0300

    Переименовал файл Api.py в TelegramBot.py Написал README.md Удалил ненужный файл Bot.py

--- Bot.py
+++
@@ -1,7 +0,0 @@
-import telegram
-bot = telegram.Bot(token='6813297671:AAGyzgBHwpQGFy-jaIdAUy98OIsbArwms18')
-chat_id = 2131163741
-user = bot.get_chat_member(chat_id=chat_id, user_id=chat_id)
-user_name = user.user.first_name
-
-bot.send_message(chat_id=2131163741, text=f"Hello, {user_name}")
