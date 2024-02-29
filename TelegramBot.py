81f0fda3d508b3e059ac509a1e7e6c5a3373267a
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2024-02-29 15:48:36 +0300

    Уточнил названия переменных

--- TelegramBot.py
+++ TelegramBot.py
@@ -15,42 +15,42 @@ def main():
     url = 'https://dvmn.org/api/long_polling/'
     current_timestamp = time.time()
 
-    counter = 0
+    counter_for_pause = 0
     while True:
-        counter += 1
+        counter_for_pause += 1
         try:
             payload = {"timestamp": current_timestamp}
 
             response = requests.get(url, headers=headers, params=payload)
             response.raise_for_status()
-            decoded_response = response.json()
-            if 'error' in decoded_response:
-                raise requests.exceptions.HTTPError(decoded_response['error'])
+            response_json = response.json()
+            if 'error' in response_json:
+                raise requests.exceptions.HTTPError(response_json['error'])
 
-            current_timestamp = decoded_response['last_attempt_timestamp']
+            current_timestamp = response_json['last_attempt_timestamp']
 
-            if decoded_response['new_attempts'][0]['is_negative']:
+            if response_json['new_attempts'][0]['is_negative']:
                 bot.send_message(chat_id=os.environ['TG_CHAT_ID'], text=f'''
-                Преподаватель проверил работу! {decoded_response['new_attempts'][0]['lesson_title']}
+                Преподаватель проверил работу! {response_json['new_attempts'][0]['lesson_title']}
                 Ну ты внатуре не баклажан. И нихуя ты не кабан
                 Непррррравильно, ёбаные волки!
-                Ссылка на урок: {decoded_response['new_attempts'][0]['lesson_url']}
+                Ссылка на урок: {response_json['new_attempts'][0]['lesson_url']}
                 ''')
             else:
                 bot.send_message(chat_id=os.environ['TG_CHAT_ID'], text=f'''
-                Преподаватель проверил работу! {decoded_response['new_attempts'][0]['lesson_title']}
+                Преподаватель проверил работу! {response_json['new_attempts'][0]['lesson_title']}
                 Ты анакондовый Джанго! Всё правильно? Ну естеееесвенно!
-                Ссылка на урок: {decoded_response['new_attempts'][0]['lesson_url']}''')
+                Ссылка на урок: {response_json['new_attempts'][0]['lesson_url']}''')
 
         except requests.exceptions.ReadTimeout:
-            if counter == 10:
+            if counter_for_pause == 10:
                 time.sleep(120)
-                counter = 0
+                counter_for_pause = 0
             continue
         except requests.exceptions.ConnectionError:
-            if counter == 10:
+            if counter_for_pause == 10:
                 time.sleep(120)
-                counter = 0
+                counter_for_pause = 0
             continue
 
 
